0db26c487eac613182c973e6b3b2d296c56dd479
goldbearman <sabirovfarit@gmail.com>
2023-12-03 21:28:53 +0300

    arr work

--- src/App.js
+++ src/App.js
@@ -8,31 +8,43 @@ import SuperSimple from "./SuperSimple";
 
 function App() {
 
+    //Проценты
+    // const percent = props.percent || 13.5;  Eslint мозг делает
+    const percent = 13.5;
+
     const sumData = {
         step:1000,
         min:15000,
-        max:300000000
+        max:500000
     }
 
     const termData = {
-        step:1,
-        min:1,
-        max:36
+        step:6,
+        min:3,
+        max:9
     }
 
     const [sum, setSum] = useState(15000);
-    const [term, setTerm] = useState(1);
+    const [term, setTerm] = useState(3);
 
-    console.log(sum);
+    //Расчеты
+    const amountEnd = sum*(1+percent/100)
+    const income = sum*percent/100
 
     return (
         <div className="App">
             Hello World
+            <input type="text" value={sum} onChange={(event)=>setSum(event.target.value)}/>
             <RangeSimple value={sum} onChange={(val)=>setSum(val)} step={sumData.step} min={sumData.min} max={sumData.max}/>
-            {/*<RangeSimple value={term} onChange={(event)=>setTerm(event.target.value)} step={termData.step} min={termData.min} max={termData.max}/>*/}
-            {/*<input type={"range"} value={term} onChange={(event)=>setTerm(event.target.value)} step={termData.step} min={termData.min} max={termData.max}/>*/}
-            {/*<input type={"text"} value={term} onChange={(event)=>setTerm(event.target.value)}/>*/}
-            {/*<SuperSimple/>*/}
+            <div style={{height: "100px"}}>промежуток</div>
+            <RangeSimple value={term} onChange={(val)=>setTerm(val)} step={termData.step} min={termData.min} max={termData.max}/>
+            <input type={"text"} value={term} onChange={(event)=>setTerm(event.target.value)}/>
+            <div style={{height: "100px"}}>промежуток</div>
+            <div>Сумма в конце срока: {amountEnd}</div>
+            <div>Доход: {income}</div>
+            <div>Ставка %годовых: {percent}</div>
+
+
         </div>
     );
 }
--- src/RangeSimple.js
+++ src/RangeSimple.js
@@ -13,7 +13,7 @@ export default function RangeSimple(props) {
             min={min}
             max={max}
             values={[value]}
-            onChange={([el]) => onChange([el])}
+            onChange={([el]) => onChange(el)}
             renderTrack={({ props, children }) => (
                 <div
                     {...props}
