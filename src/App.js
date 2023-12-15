1f47a6a63fc8a9ece3ef4db03c1498b4b1338c05
goldbearman <sabirovfarit@gmail.com>
2023-12-15 17:39:32 +0300

    calk work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 import RangeSimple from "./RangeSimple";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import SuperSimple from "./SuperSimple";
 import { SliderEditInput } from "./SliderEditInput/SliderEditInput";
 import { CustomRange } from "./CustomRange";
@@ -9,90 +9,39 @@ import { CustomRange } from "./CustomRange";
 
 function App() {
 
-  //Проценты
-  // const percent = props.percent || 13.5;  Eslint мозг делает
-  const percent = 13.5;
-
-  const sumData = {
-    step: 1000,
-    min: 15000,
-    max: 500000
+  const percentages = {
+    3: 15,
+    9: 13.5,
   }
 
-  const termData = {
-    step: 6,
-    min: 3,
-    max: 9
-  }
-
-  const [sum, setSum] = useState(15000);
+  const [sum, setSum] = useState(100000);
   const [term, setTerm] = useState(3);
 
   //Расчеты
-  const amountEnd = sum * (1 + percent / 100)
-  const income = sum * percent / 100
+  const amountEnd = Math.round(sum * (1 + percentages[term] / 100))
+  const income = sum * percentages[term] / 100
 
-  console.log(sum);
-  console.log(typeof sum);
+  const onInputChangeSum = useCallback((event) => {
+    setSum(event.target.value)
+  }, []);
 
-  function checkInput(event) {
-    console.log(event.target.value);
-    console.log(typeof event.target.value);
-    console.log(Number(event.target.value.replace(/\s/g, "")));
-    if (isNaN(Number(event.target.value.replace(/\s/g, "")))) {
-      console.log('не число')
-      return sum;
-    } else Number(event.target.value.replace(/\s/g, ""))
-  }
-
-  function checkInput2(event) {
-    console.log(event.target.value)
-    // return event.target.value.replace(/[^0-9]/, '');
-    // event.target.value = event.target.value.replace(/[^0-9 \,]/, '');
-    event.target.value = event.target.value.replace(/\s/g, "")
-    console.log(event.target.value)
-    event.target.value = event.target.value.replace(/[^0-9]/, '');
-    console.log(event.target.value)
-    console.log(typeof event.target.value)
-    if (+event.target.value > 300000000) {
-      event.target.value = '300000000'
-    }
-  }
-
-  // function maxNumber(event){
-  //   +(event.target.value.replace(/\s/g, ""))
-  // }
+  const onInputChangeTerm = useCallback((event) => {
+    setTerm(event.target.value)
+  }, []);
 
 
   return (
     <div className="App">
-      Hello World
-      <input type="text" maxLength={11}
-             value={sum.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1 ')}
-             onInput={event => checkInput2(event)}
-             onChange={(event) => setSum(+(event.target.value.replace(/\s/g, "")))}/>
-
-      <input className="form-control  text-center"
-             onInput={event => checkInput2(event)}
-             type="text" id="telefonlar" name="telefonlar" required
+      Калькулятор
+      <CustomRange onChange={onInputChangeSum} value={sum}
+                   min={15000} max={500000} step={1000}
       />
+      <CustomRange onChange={onInputChangeTerm} value={term} min={3} max={9} step={6} defaultValue={3}
+                   onlyRead={false}/>
 
-      {/*<input type="text" required*/}
-      {/*       value={sum.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1 ')}*/}
-      {/*       onChange={(event) => setSum(checkInput(event))}/>*/}
-      <SliderEditInput value={sum} renderValue={sum} inputSuffix={0} onInputChange onFinalChange ariaLabel />
-      <RangeSimple value={sum} onChange={(val) => setSum(val)} step={sumData.step} min={sumData.min} max={sumData.max}/>
-      <div style={{ height: "100px" }}>промежуток</div>
-      <RangeSimple value={term} onChange={(val) => setTerm(val)} step={termData.step} min={termData.min}
-                   max={termData.max}/>
-      <input type={"text"} value={term} onChange={(event) => setTerm(event.target.value)}/>
-      <div style={{ height: "100px" }}>промежуток</div>
       <div>Сумма в конце срока: {amountEnd.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1 ')}</div>
       <div>Доход: {income.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1 ')}</div>
-      <div>Ставка %годовых: {percent}</div>
-      <div style={{ height: "100px" }}>промежуток</div>
-      <CustomRange/>
-
+      <div>Ставка %годовых: {percentages[term]}</div>
     </div>
   );
 }
--- src/CustomRange/index.js
+++ src/CustomRange/index.js
@@ -1,68 +1,47 @@
 import './styles.css';
-import { useCallback, useLayoutEffect, useRef, useState } from "react";
-import * as PropTypes from "prop-types";
-import { ReactComponent as NotActiveIcon } from '../static/images/icon/form/icon-edit.svg'
+import {  useRef, useState } from "react";
 import { ReactComponent as ActiveIcon } from '../static/images/icon/form/icon-edit-active.svg'
 
 
-export const CustomRange = () => {
-
-  const min = 15000;
-  const max = 500000;
-
+export const CustomRange = ({onChange,value,min,max,step,onlyRead=true}) => {
 
   const input1 = useRef(null);
 
-  const defaultValue = 100000;
-
   const [type, setType] = useState("text");
-
-  const [value, setValue] = useState(defaultValue);
   const [isActiveIcon, setIsActiveIcon] = useState(true);
-  console.log(value);
-  console.log(isActiveIcon);
 
   const onFocus = () => {
     console.log(isActiveIcon)
   }
-  const onBlur = () => setIsActiveIcon(true)
-
-  const onInputChange = useCallback((event) => {
-    console.log(typeof event.target.value)
-    if (event.target.value > max) {
-      setValue(max)
-    } else setValue(event.target.value);
-  }, []);
+  const onBlur = () => {
+    setIsActiveIcon(true)
+    setType("text");
+  }
 
   const handleClick1 = () => {
     setIsActiveIcon(false);
     setType("number");
     input1.current.focus();
-
   }
 
   //Значение на выходе
-  let finalValue = (type)=>{
-    console.log('finalValue')
-    if(type==="text") return  value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1 ')
-    if(type==="number") return  value
+  let finalValue = (type) => {
+    if (type === "text") return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1 ')
+    if (type === "number") return value
   }
 
-  console.log(value);
-  console.log(typeof value);
+  console.log(type);
 
   return (
-
     <div className="RangeContainer">
       <div className="RangeContainer__title">Сумма при открытии счета</div>
       <div className="RangeContainer__value">
         <input type={type} ref={input1}
-          // onInput={checkInputNumber}
                readOnly={isActiveIcon}
                onFocus={onFocus} onBlur={onBlur}
                min={min} max={max}
-               value={finalValue(type)} onChange={onInputChange}/>
-        {isActiveIcon &&
+               value={finalValue(type)} onChange={onChange}/>
+        {onlyRead && isActiveIcon &&
           <div disabled={!isActiveIcon} onClick={handleClick1}>
 
             <ActiveIcon/>
@@ -70,11 +49,12 @@ export const CustomRange = () => {
           </div>}
       </div>
       <div className="RangeContainer__range">
-        <input type="range" value={value === '' ? 15000 : value} onChange={event => setValue(event.target.value)}
-               step={1000} min={min} max={max}/>
+        <input type="range" value={value === '' ? 15000 : value} onChange={event =>onChange(event)}
+               // onChange={event => setValue(event.target.value)}
+               step={step} min={min} max={max}/>
         <div className="RangeContainer__minMaxValues">
-          <div className="value_left">15000</div>
-          <div className="value_right">500000</div>
+          <div className="value_left">{min}</div>
+          <div className="value_right">{max}</div>
 
         </div>
       </div>
